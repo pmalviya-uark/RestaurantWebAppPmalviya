9d3cc68f99d70f4185e66a04618dffbf662bb7a8
Poornima Malviya <pmalviya@walton.uark.edu>
2025-04-14 18:57:36 -0500

    Update settings.py
    Fix ALLOWED_HOSTS for Azure deployment

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restaurantwebappa3-h2e0g2b5d2gqema5.eastus-01.azurewebsites.net']
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
